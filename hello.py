774c6a80d68175cb7d8ba4c3f580df568cd9b100
fckiblessme <wcbrtidkw@gmail.com>
2026-02-03 15:19:51 +0300

    Finish

--- hello.py
+++ hello.py
@@ -3,3 +3,17 @@ s = "*"*30
 print(s)
 print("New project")
 print(s)
+def generate_squares(n):
+    """Generate list of squares using list comprehension"""
+    return [x ** 2 for x in range(n)]
+
+def generate_squares(n):
+    """Generate list of squares using for loop"""
+    result = []
+    for x in range(n):
+        result.append(x ** 2)
+    return result
+
+if __name__ == "__main__":
+    print("For loop version:")
+    print(f"generate_squares(5) = {generate_squares(5)}")
